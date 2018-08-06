a80382d58783bd63d02dc0337da084601a160956
Oneleven <wuduyouwo@gmail.com>
2018-08-06 23:19:46 +0800

    勾勒自己歌曲列表的dom轮廓

---
+++ src/components/my-playlist/my-playlist.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="my-songlist-wrapper">
+      <div class="songlist-wrapper">
+        <div class="songlist-header">
+          <img src="" alt="">
+          <h1>顺序播放</h1>
+          <img src="" alt="">
+        </div>
+        <div class="list-content">
+          <ul>
+            <li>
+              <img src="" alt="">
+              <p>歌曲名字</p>
+              <img src="" alt="">
+              <img src="" alt="">
+            </li>
+          </ul>
+        </div>
+        <div class="list-operate">
+          <div class="list-add">
+            <img src="" alt="">
+            <span>添加歌曲到列表</span>
+          </div>
+        </div>
+        <div class="close">
+          <p>关闭</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-playlist'
+}
+</script>
+
+<style scoped lang='stylus'>
+@import '~common/css/global.styl'
+@import '~common/css/mixin.styl'
+
+</style>
