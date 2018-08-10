342b0ae3635e7940ddb61e29265d2b40952da21e
Oneleven <wuduyouwo@gmail.com>
2018-08-10 12:17:11 +0800

    完成最后的用户界面，项目大体完成，待编译打包，发送至服务器

--- src/components/user-center/user-center.vue
+++ src/components/user-center/user-center.vue
@@ -8,23 +8,38 @@
         <tab :tabs="tabs" :currentIndex="currentIndex" @switch="switchItem"></tab>
       </div>
       <div class="play">
-        <div class="play-wrapper">
+        <div class="play-wrapper" @click="randomPlay">
           <svg class="icon-bofang" aria-hidden="true">
             <use xlink:href="#icon-bofang"></use>
           </svg>
           <span>随机播放全部</span>
         </div>
       </div>
-      <div class="tab-content"></div>
+      <div class="tab-content" ref="tabContent">
+        <scroll v-show="currentIndex === 0" :data="favouriteList" class="favouriteScroll" ref="favouriteScroll">
+          <song-list :songs="favouriteList" @playSong="playSong"></song-list>
+        </scroll>
+        <scroll v-show="currentIndex === 1" :data="songHistory" class="songScroll" ref="songScroll">
+          <song-list :songs="songHistory" @playSong="playSong"></song-list>
+        </scroll>
+      </div>
+      <no-result v-show="noResult" :wrong="text" class="no-result"></no-result>
     </div>
   </transition>
 </template>
 
 <script>
 import Tab from 'base/tab/tab'
+import NoResult from 'base/no-result/no-result'
+import { mapGetters, mapActions } from 'vuex'
+import Scroll from 'base/scroll/scroll'
+import SongList from 'base/song-list/song-list'
+import { playlistMixin } from 'common/js/mixin'
 
 export default {
   name: 'user-center',
+
+  mixins: [playlistMixin],
 
   data () {
     return {
@@ -36,17 +51,75 @@ export default {
     }
   },
 
+  computed: {
+    ...mapGetters([
+      'favouriteList',
+      'songHistory'
+    ]),
+
+    noResult () {
+      if (this.currentIndex === 0) {
+        return !this.favouriteList.length
+      } else {
+        return !this.songHistory.length
+      }
+    },
+
+    text () {
+      if (this.currentIndex === 0) {
+        return '>.< 你还未加添喜欢的歌曲哦'
+      } else {
+        return '>.< 暂无搜索历史'
+      }
+    }
+  },
+
   components: {
-    Tab
+    Tab,
+    Scroll,
+    SongList,
+    NoResult
   },
 
   methods: {
+    ...mapActions([
+      'insertSong',
+      'random'
+    ]),
+
     back () {
       this.$router.back()
     },
 
     switchItem (index) {
       this.currentIndex = index
+      this.$nextTick(() => {
+        this.$refs.songScroll.refresh()
+      })
+    },
+
+    playSong (song) {
+      this.insertSong(song)
+    },
+
+    // 点击播放随机歌曲
+    randomPlay () {
+      let list = this.currentIndex === 0 ? this.favouriteList : this.songHistory
+      console.log(list)
+      this.random({list})
+    },
+
+    // 修改miniplayer遮挡问题
+    handlePlaylist () {
+      let bottom = this.playlist.length > 0 ? '60' : ''
+      this.$refs.tabContent.style.height = 'calc(100vh - 2.18rem - ' + bottom + 'px)'
+      // if (this.currentIndex === 0) {
+      //   this.$refs.favouriteScroll.refresh()
+      // } else {
+      //   this.$refs.songScroll.refresh()
+      // }
+      this.$refs.favouriteScroll && this.$refs.favouriteScroll.refresh()
+      this.$refs.songScroll && this.$refs.songScroll.refresh()
     }
   }
 }
@@ -64,7 +137,12 @@ export default {
   right 0
   background-color $backcolor
   .tab
-    padding .2rem 0 .5rem 0
+    padding .2rem 0 .4rem 0
+  .tab-content
+    height calc(100vh - 2.18rem)
+    overflow hidden
+    .favouriteScroll, .songScroll
+      height 100%
   .icon
     width .35rem
     height .35rem
@@ -75,6 +153,7 @@ export default {
   .play
     height .64rem
     width 100%
+    padding-bottom .5rem
     .play-wrapper
       display flex
       align-items center
@@ -93,6 +172,11 @@ export default {
         color $maincolor
         font-size .24rem
         padding-right .2rem
+  .no-result
+    position absolute
+    top 2.38rem
+    left 0
+    height 70%
 .icon
   iconfontStyle()
 
